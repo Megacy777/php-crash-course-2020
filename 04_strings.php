dc302836ea89289b24f1abc8bcf60cb78b8a5014
Megacy777 <megacy777@gmail.com>
2023-10-26 15:25:52 +0300

    Update 04_strings.php
    Array and Strings

--- 04_strings.php
+++ 04_strings.php
@@ -1,13 +1,60 @@
 <?php
 
-// Create simple string
-
+// // // Create simple string
+ $string = "Hello, I am the one"; 
+$string2 = 'world, here comes magic'; 
+echo $string . '<br>';
+echo $string2 . '<br>';
 // String concatenation
+echo "Hello " . " Mama".'<br>'; 
+echo $string.$string2;
 
 // String functions
+echo "1 - " . strlen($string) . '<br>' . PHP_EOL;
+echo "2 - " . trim($string) . '<br>' . PHP_EOL;
+echo "3 - " . ltrim($string) . '<br>' . PHP_EOL;
+echo "4 - " .rtrim($string) . '<br>' . PHP_EOL;
+echo "5 - " . str_word_count($string) . '<br>' . PHP_EOL;
+echo "6 - " . strrev($string) . '<br>' . PHP_EOL;
+echo "7 - " . strtoupper($string) . '<br>' . PHP_EOL;
+echo "8 - " . strtolower($string) . '<br>' . PHP_EOL;
+echo "9 - " . ucfirst('hello') . '<br>' . PHP_EOL;
+echo "10 - " . lcfirst('HELLO') . '<br>' . PHP_EOL;
+echo "11 - " . ucwords('hello world') . '<br>' . PHP_EOL;
+echo "12 - " . strpos($string, 'world') . '<br>' . PHP_EOL; // Change into world
+echo "13 - " . stripos($string, 'world') . '<br>' . PHP_EOL;
+echo "14 - " . substr($string, 8) . '<br>' . PHP_EOL;
+echo "15 - " . str_replace('World', 'PHP', $string) . '<br>' . PHP_EOL;
+echo "16 - " . str_ireplace('world', 'PHP', $string) . '<br>' . PHP_EOL;
+
+$invoiceNumber = 100;
+$invoiceNumber2 = 123456;
+echo str_pad($invoiceNumber, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
+echo str_pad($invoiceNumber2, 8, '0', STR_PAD_LEFT) . '<br>' . PHP_EOL;
+echo str_repeat('Hello', 4) . '<br>' . PHP_EOL;
 
 // Multiline text and line breaks
+$longText = "
+  Hello, my name is Massawe
+  I am 27,
+  I love my daughter
+";
+ echo $longText . '<br>' . PHP_EOL;
+ echo nl2br($longText) . '<br>' . PHP_EOL;
+
 
 // Multiline text and reserve html tags
+$longText = "
+Hello, my name is <b>Massawe</b>
+I am <b>27</b>,
+I love my daughter
+";
+echo "1 - " . $longText . '<br>';
+echo "2 - " . nl2br($longText) . '<br>';
+echo "3 - " . htmlentities($longText) . '<br>' . PHP_EOL;
+echo "4 - " . nl2br(htmlentities($longText)) . '<br>' . PHP_EOL;
+echo "5 - " . html_entity_decode(htmlentities($longText)) . '<br>' . PHP_EOL;
+echo "6 - " . htmlspecialchars($longText) . '<br>' . PHP_EOL;
+
 
 // https://www.php.net/manual/en/ref.strings.php
