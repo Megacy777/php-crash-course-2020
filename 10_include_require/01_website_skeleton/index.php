a582a6882dcc1643309fc52a18165add2aab32f7
Megacy777 <megacy777@gmail.com>
2023-10-27 16:32:49 +0300

    Update index.php

--- 10_include_require/01_website_skeleton/index.php
+++ 10_include_require/01_website_skeleton/index.php
@@ -12,7 +12,7 @@
     <a href="index.php">Home</a>
     <a href="about.php">About</a>
 </header>
-<h3>Georgia, Tbilisi 5&#8451;</h3>
+<h3>Well here we go again 5&#8451;</h3>
 <h1>Welcome to my cool website</h1>
 <footer>
     Copyright &copy; 2023 MEGACY
