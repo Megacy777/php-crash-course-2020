d3b34431872a82f02c2f463cc531d29c5492b927
Megacy777 <megacy777@gmail.com>
2023-10-27 16:32:54 +0300

    Create 01_search_form.php

---
+++ 18_superglobals/01_get_post/01_search_form.php
@@ -0,0 +1,30 @@
+<?
+
+echo '<pre>';
+var_dump($_GET)
+// echo '</pre>';
+<br>
+$keyword = $_GET['keyword'];
+echo $keyword;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+<div class = "form group">
+<input class = "form-group" type = "text" name = "keyword"
+    placeholder = "Type and hit hit enter">
+    <form class = "form-group" action="<?php ?>" method="get">
+    </form>
+</div>
+<button class = "btn btn-primary">Search</button>
+
+</body>
+</html>
+
