9e766de407474d2e872dcd984b8f527d151c6b7e
Megacy777 <megacy777@gmail.com>
2023-11-02 18:13:07 +0300

    Update 02_registration_form.php

---
+++ 18_superglobals/01_get_post/02_registration_form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Insider Details </h2>
+<form action="" method="post" novalidate>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br>
+
+  <label for="password">Password:</label><br>
+  <input type="text" id="pwd" name="pword" value=""><br>
+  <br>
+ 
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+</body>
+</html>
+
